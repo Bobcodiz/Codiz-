e484a2ee1d7b184e8bafc44af464add0bfc4ac28
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-03-11 15:44:19 +0300

    created staff registration model

---
+++ resource/src/main/java/com/codiz/resource/models/StaffModel.java
@@ -0,0 +1,20 @@
+package com.codiz.resource.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class StaffModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+}
---
+++ resource/src/main/java/com/codiz/resource/repository/StaffRepository.java
@@ -0,0 +1,9 @@
+package com.codiz.resource.repository;
+
+import com.codiz.resource.models.StaffModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffRepository extends JpaRepository<StaffModel,Long> {
+}
