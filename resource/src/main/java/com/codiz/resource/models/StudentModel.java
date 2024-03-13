1fb08985b2009c9f160ea55079b8bee041e110b6
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-03-13 14:56:48 +0300

    working on the student model

---
+++ resource/src/main/java/com/codiz/resource/models/StudentModel.java
@@ -0,0 +1,16 @@
+package com.codiz.resource.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class StudentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
