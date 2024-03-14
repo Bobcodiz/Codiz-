d4285a90aa3a370a2816e98aefd027978b21ecba
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-03-14 10:46:21 +0300

    service to register students

---
+++ resource/src/main/java/com/codiz/resource/dto/StudentDetails.java
@@ -0,0 +1,10 @@
+package com.codiz.resource.dto;
+
+import lombok.Data;
+
+@Data
+public class StudentDetails {
+    private String firstName;
+    private String lastName;
+    private String grade;
+}
--- resource/src/main/java/com/codiz/resource/services/StudentService.java
+++ resource/src/main/java/com/codiz/resource/services/StudentService.java
@@ -1,4 +1,30 @@
 package com.codiz.resource.services;
 
+import com.codiz.resource.dto.StudentDetails;
+import com.codiz.resource.models.StudentModel;
+import com.codiz.resource.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentModel studentModel(StudentDetails details)
+    {
+        log.info("service to insert student details");
+        StudentModel model = new StudentModel();
+        try {
+            model.setFirstName(details.getFirstName());
+            model.setLastName(details.getLastName());
+            model.setGrade(details.getGrade());
+        }catch (Exception e){
+            throw new RuntimeException("could not insert student");
+        }
+        return studentRepository.save(model);
+    }
 }
